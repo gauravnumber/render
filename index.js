691935c54e5410fa3d819fbd6af9647179b502dd
Gaurav Singh <gauravnumber@gmail.com>
2022-09-22 21:55:01 +0530

    email when tomorrow will raining

--- index.js
+++ index.js
@@ -27,44 +27,47 @@ app.get("/weather", async (req, res) => {
   const description = tomorrow.weather.description;
   const isRain = weather >= 200 && weather <= 522;
 
-  const client = Sib.ApiClient.instance;
-  const apiKey = client.authentications["api-key"];
-  apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
+  if (isRain) {
+    const client = Sib.ApiClient.instance;
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
 
-  const tranEmailApi = new Sib.TransactionalEmailsApi();
+    const tranEmailApi = new Sib.TransactionalEmailsApi();
 
-  const sender = {
-    email: process.env.SENDER_EMAIL,
-    name: "Gaurav",
-  };
+    const sender = {
+      email: process.env.SENDER_EMAIL,
+      name: "Gaurav",
+    };
 
-  const receivers = [
-    {
-      email: process.env.RECEIVER_EMAIL,
-    },
-  ];
+    const receivers = [
+      {
+        email: process.env.RECEIVER_EMAIL,
+      },
+    ];
 
-  tranEmailApi
-    .sendTransacEmail({
-      sender,
-      to: receivers,
-      subject: `Tomorrow will be raining.`,
-      textContent: `
+    tranEmailApi
+      .sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: `Tomorrow will be raining.`,
+        textContent: `
         It may be {{params.description}}. Chances of raining is {{params.percentage}}%
         `,
-      // htmlContent: `
-      //   <h1>Cules Coding</h1>
-      //   <a href="https://cules-coding.vercel.app/">Visit</a>
-      //           `,
-      params: {
-        description: tomorrow.weather.description,
-        percentage: tomorrow.pop,
-      },
-    })
-    .then(console.log)
-    .catch(console.log);
+        // htmlContent: `
+        //   <h1>Cules Coding</h1>
+        //   <a href="https://cules-coding.vercel.app/">Visit</a>
+        //           `,
+        params: {
+          description: tomorrow.weather.description,
+          percentage: tomorrow.pop,
+        },
+      })
+      .then(console.log)
+      .catch(console.log);
 
-  console.log(tomorrow);
+    console.log(tomorrow);
+  }
+
   res.json(list);
 });
 
